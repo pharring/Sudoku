65061c392eaa41eeee5c3636d02d839146f4d979
Paul Harrington <pharring@microsoft.com>
2019-09-05 19:50:17 -0700

    It works

--- Sudoku/Program.cs
+++ Sudoku/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 
 namespace Sudoku
 {
@@ -6,7 +7,29 @@ namespace Sudoku
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            //var puzzle = new Sudoku(".................................................................................");
+            //var puzzle = new Sudoku("987654321........................................................................");
+            Go(".623.7...3.5.9.......6......79....35.2.7.4.9.54....28......3.......6.7.1...5.986.");
+            Go("8..........36......7..9.2...5...7.......457.....1...3...1....68..85...1..9....4..");
+            Go(".8....15.4.65.9.8......8..............2.4...33..8.1...9...7....6.......415.....9.");
+        }
+
+        static void Go(string puzzle)
+        {
+            var sudoku = new Sudoku(puzzle);
+            var sw = Stopwatch.StartNew();
+            var solved = sudoku.Solve();
+            sw.Stop();
+            if (solved)
+            {
+                Console.WriteLine("{0} solved in {1:f2}ms", puzzle, sw.Elapsed.TotalMilliseconds);
+                Console.WriteLine(sudoku.ToString());
+            }
+            else
+            {
+                Console.WriteLine("No solution found.");
+            }
         }
     }
 }
---
+++ Sudoku/Sudoku.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Text;
+
+namespace Sudoku
+{
+    public class Sudoku
+    {
+        private static readonly IndexRowColumnBlock[] s_map = new IndexRowColumnBlock[81];
+
+        private readonly struct IndexRowColumnBlock
+        {
+            public readonly byte Index;
+            public readonly byte Row;
+            public readonly byte Col;
+            public readonly byte Block;
+
+            public IndexRowColumnBlock(byte index)
+            {
+                Index = index;
+                Row = (byte)(index / 9);
+                Col = (byte)(index % 9);
+                Block = (byte)(3 * (Row / 3) + (Col / 3));
+            }
+        }
+
+        static Sudoku()
+        {
+            for (byte i = 0; i < 81; i++)
+            {
+                s_map[i] = new IndexRowColumnBlock(i);
+            }
+        }
+
+        private readonly byte[] _board = new byte[81];
+        private readonly ushort[] _rowBits = new ushort[9];
+        private readonly ushort[] _colBits = new ushort[9];
+        private readonly ushort[] _blockBits = new ushort[9];
+
+        public Sudoku(string puzzle)
+        {
+            if (puzzle.Length != 81)
+            {
+                throw new ArgumentException("puzzle must be exactly 81 chars");
+            }
+
+            for (int i = 0; i < 81; i++)
+            {
+                var ch = puzzle[i];
+                if (ch >= '0' && ch <= '9')
+                {
+                    SetValue(s_map[i], (byte)(ch - '0'));
+                }
+            }
+        }
+
+        public bool Solve() => Solve(default);
+
+        private bool Solve(int idx)
+        {
+            for (; idx < 81; idx++)
+            {
+                if (!IsOccupied(idx))
+                {
+                    ref var position = ref s_map[idx];
+                    var available = Available(position);
+                    if (available != 0)
+                    {
+                        for (byte trialValue = 1; trialValue <= 9; trialValue++)
+                        {
+                            available >>= 1;
+
+                            if ((available & 1) != 0)
+                            {
+                                SetValue(position, trialValue);
+                                if (Solve(idx + 1))
+                                {
+                                    return true;
+                                }
+                            }
+                        }
+
+                        SetValue(position, 0);
+                    }
+
+                    return false;
+                }
+            }
+
+            // Solution found
+            return true;
+        }
+
+        private void SetValue(in IndexRowColumnBlock position, byte val)
+        {
+            ref byte slot = ref _board[position.Index];
+            var oldVal = slot;
+            slot = val;
+
+            if (oldVal != 0)
+            {
+                var clearMask = (ushort)~(1 << oldVal);
+                _rowBits[position.Row] &= clearMask;
+                _colBits[position.Col] &= clearMask;
+                _blockBits[position.Block] &= clearMask;
+            }
+
+            if (val != 0)
+            {
+                var setMask = (ushort)(1 << val);
+                _rowBits[position.Row] |= setMask;
+                _colBits[position.Col] |= setMask;
+                _blockBits[position.Block] |= setMask;
+            }
+        }
+
+        private bool IsOccupied(int position) => _board[position] != 0;
+
+        private int Available(in IndexRowColumnBlock position) => ~(_rowBits[position.Row] | _colBits[position.Col] | _blockBits[position.Block]);
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            sb.AppendLine("╔═══╤═══╤═══╦═══╤═══╤═══╦═══╤═══╤═══╗");
+
+            for (int row = 0; row < 9; row++)
+            {
+                sb.Append("║ ");
+                for (int col = 0; col < 9; col++)
+                {
+                    var val = _board[row * 9  + col];
+                    sb.Append(val == 0 ? '.' : (char)(val + '0'));
+
+                    if (col == 2 || col == 5)
+                    {
+                        sb.Append(" ║ ");
+                    }
+                    else if (col == 8)
+                    {
+                        sb.AppendLine(" ║");
+                    }
+                    else
+                    {
+                        sb.Append(" │ ");
+                    }
+                }
+
+                if (row == 2 || row == 5)
+                {
+                    sb.AppendLine("╠═══╪═══╪═══╬═══╪═══╪═══╬═══╪═══╪═══╣");
+                }
+                else if (row == 8)
+                {
+                    sb.AppendLine("╚═══╧═══╧═══╩═══╧═══╧═══╩═══╧═══╧═══╝");
+                }
+                else
+                {
+                    sb.AppendLine("╟───┼───┼───╫───┼───┼───╫───┼───┼───╢");
+                }
+            }
+
+            return sb.ToString();
+        }
+    }
+}
